d7f51dafdb54a5c97038335230442a8bf34d0aa5
Kurt Biery <biery@daq.fnal.gov>
2026-04-14 08:41:43 -0500

    Changed the verbosity level for required message printout in log_file_checks.py so that it appears at a slightly lower level.

--- src/integrationtest/log_file_checks.py
+++ src/integrationtest/log_file_checks.py
@@ -72,13 +72,13 @@ def log_has_no_errors(log_file_name, print_logfilename_for_problems=True, exclud
             print(f"\N{POLICE CARS REVOLVING LIGHT} Failure: Required log message \"{substr}\" was not found in {log_file_name} \N{POLICE CARS REVOLVING LIGHT}")
             ok=False
         elif print_required_message_report:
-            if verbosity_helper.compare_level(IntegtestVerbosityLevels.integtest_debug):
+            if verbosity_helper.compare_level(IntegtestVerbosityLevels.drunc_transitions):
                 print(f"\N{WHITE HEAVY CHECK MARK} Required log message \"{substr}\" occurred {count} times in {log_file_name}")
         overall_required_message_count += count
         if count > 0:
             found_message_count += 1
     if overall_required_message_count > 0:
-        if verbosity_helper.compare_level(IntegtestVerbosityLevels.integtest_debug):
+        if verbosity_helper.compare_level(IntegtestVerbosityLevels.drunc_transitions):
             print(f"\N{WHITE HEAVY CHECK MARK} Note: required log messages were found in {overall_required_message_count} lines in {log_file_name} based on {found_message_count} required messages (of a total of {len(required_substring_list)} required messages).")
     return ok
 
