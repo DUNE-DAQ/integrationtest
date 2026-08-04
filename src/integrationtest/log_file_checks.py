45c9f951d70dbac1f4f655e4c17ecb7177c45c0a
Kurt Biery <biery@daq.fnal.gov>
2026-08-04 11:36:31 -0500

    Fixed merge errors in log_file_checks.py

--- src/integrationtest/log_file_checks.py
+++ src/integrationtest/log_file_checks.py
@@ -116,13 +116,13 @@ def logs_are_error_free(log_file_names, show_all_problems=True, print_logfilenam
             ["LogLevel=error", r'key:\s+"DUNEDAQ_ERS_',
              r"drunc.utils.ConnectivityServiceClient\s+404 Client Error: NOT FOUND for url:"]
         )
-        local_excl_string_map.setdefault("drunc-unified_shell", []).extend(
+        local_excl_string_map.setdefault("drunc-unified-shell", []).extend(
             ["LogLevel=error", r'key:\s+"DUNEDAQ_ERS_', r"DUNEDAQ_ERS_.*erstrace", "export DUNEDAQ_ERS_",
              r"NewConnectionError.* Failed to establish a new connection: \[Errno 111\] Connection refused",
              r"drunc.utils.ConnectivityServiceClient\s+404 Client Error: NOT FOUND for url:"]
         )
-        excluded_substring_map.setdefault("drunc-process-manager", []).extend(
-            ["LogLevel=error", "key:\s\"DUNEDAQ_ERS_", "DUNEDAQ_ERS_.*erstrace", "export DUNEDAQ_ERS_"]
+        local_excl_string_map.setdefault("drunc-process-manager", []).extend(
+            ["LogLevel=error", r'key:\s+"DUNEDAQ_ERS_', r"DUNEDAQ_ERS_.*erstrace", "export DUNEDAQ_ERS_"]
         )
 
     # 21-Jul-2026, KAB: phrases that we always want to exclude
