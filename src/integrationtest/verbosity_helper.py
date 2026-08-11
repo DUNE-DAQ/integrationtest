10b5710fe0c5b7b1db8d279741ba22c584f44a90
Kurt Biery <biery@daq.fnal.gov>
2026-08-10 21:33:17 -0500

    Updated the descriptive text for verbosity levels 4 and 5 in verbosity_helper.py.

--- src/integrationtest/verbosity_helper.py
+++ src/integrationtest/verbosity_helper.py
@@ -6,8 +6,8 @@ class IntegtestVerbosityLevels:
     just_errors_and_warnings: int = 1  # shows just errors and warnings
     drunc_boot_terminate: int = 2  # adds a small subset of drunc transitions
     drunc_transitions: int = 3  # adds all drunc transitions plus validation check results
-    integtest_debug: int = 4  # adds ResourceValidation debug info and any other integtest debug
-    full_output: int = 5  # shows everything
+    integtest_debug: int = 4  # adds ResourceValidation debug info and other integtest debug
+    full_output: int = 5  # shows all drunc output
     drunc_debug: int = 6  # enables drunc debug messages
 
 class VerbosityHelper:
